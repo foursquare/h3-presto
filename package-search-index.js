2bf45ce9af5a8ebfeba7a35c7e5ee2b884c1cda8
isaacbrodsky <isaacbrodsky@users.noreply.github.com>
2022-10-14 19:29:09 +0000

    deploy: ee6bd18ff927d9668a2620364b24daf8be9dcfe7

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.foursquare.presto.h3"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.foursquare.presto.h3","l":"CellToParentFunction"},{"p":"com.foursquare.presto.h3","l":"H3Plugin"},{"p":"com.foursquare.presto.h3","l":"LatLngToCellFunction"}];updateSearchResults();
