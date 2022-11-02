d4332ba2a162d30a412e058a5ffcd42be0dde782
Isaac Brodsky <isaac@unfolded.ai>
2022-11-02 14:51:11 -0700

    restore cellToParent

--- src/main/java/com/foursquare/presto/h3/H3Plugin.java
+++ src/main/java/com/foursquare/presto/h3/H3Plugin.java
@@ -47,7 +47,7 @@ public class H3Plugin implements Plugin {
   @Override
   public Set<Class<?>> getFunctions() {
     return ImmutableSet.<Class<?>>builder()
-        .add(IndexingFunctions.class, InspectionFunctions.class)
+        .add(IndexingFunctions.class, CellToParentFunction.class, InspectionFunctions.class)
         .build();
   }
 }
